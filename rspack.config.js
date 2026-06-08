79263fc05fb414a3a47d3b518958f16e96ac4b17
Sevastyan Kalgin <foxhable@gmail.com>
2026-06-08 11:49:13 +0300

    chore: rspack, reset css styles, scss and html resolvers

---
+++ rspack.config.js
@@ -0,0 +1,40 @@
+// @ts-check
+import { defineConfig } from "@rspack/cli";
+import { rspack } from "@rspack/core";
+import * as path from 'path'
+
+// Docs: https://rsbuild.rs/config/
+export default defineConfig({
+  entry: {
+    index: './src/pages/index/index.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(sass|scss)$/,
+        use: [
+          {
+            loader: "sass-loader",
+            options: {
+              api: "modern-compiler",
+              implementation: require.resolve("sass-embedded"),
+            },
+          },
+        ],
+        type: "css/auto",
+      },
+    ],
+  },
+  resolve: {
+    alias: {
+      'src': path.resolve(__dirname, 'src'),
+    },
+  },
+  html: {
+    template: './src/pages/[name]/[name].html',
+  },
+  plugins: [new rspack.HtmlRspackPlugin({
+
+    minify: false,
+  })],
+});
