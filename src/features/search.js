8bc5652801bdb1a2903adc6221499f95334a514b
Sevastyan Kalgin <foxhable@gmail.com>
2026-06-09 18:41:48 +0300

    feat: search

--- src/features/search.js
+++ src/features/search.js
@@ -1,3 +1,21 @@
-export function initSearchField() {
+import { UrlChangeEvent } from 'src/constants/courses.js'
+import { clearCoursesList, mountCoursesListToDOM } from 'src/components/courseCard.js'
 
+/** @type {HTMLInputElement} */
+const searchFieldEl = document.querySelector('.list-section__search-input')
+
+/** @param {Course[]} coursesList */
+export function initSearchField(coursesList) {
+    if (!searchFieldEl) return
+
+    searchFieldEl.addEventListener('input', () => {
+        if (!searchFieldEl.value) return window.dispatchEvent(UrlChangeEvent)
+
+        const foundedItems = coursesList.filter((el) => {
+          return el.title.toLowerCase().includes(searchFieldEl.value.toLowerCase())
+        })
+
+        clearCoursesList()
+        mountCoursesListToDOM(foundedItems)
+    })
 }
--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,6 +1,7 @@
 import { updateFilter, setFilterButtonsNum } from 'src/features/filters.js'
 import { initFilterButton, setPage, updateLoadMoreButton } from 'src/features/pagination.js'
 import { URL_CHANGE_EVENT, UrlChangeEvent } from 'src/constants/courses.js'
+import { initSearchField } from 'src/features/search.js'
 
 document.addEventListener('DOMContentLoaded', async () => {
     /** @type {import('./constants/courses.js').Course[]} */
@@ -13,10 +14,12 @@ document.addEventListener('DOMContentLoaded', async () => {
         setPage(1)
         window.dispatchEvent(UrlChangeEvent)
     })
+
     window.addEventListener(URL_CHANGE_EVENT, () => {
         const filteredCourses = updateFilter(courses)
         updateLoadMoreButton(filteredCourses)
     })
 
     initFilterButton(courses)
+    initSearchField(courses)
 })
