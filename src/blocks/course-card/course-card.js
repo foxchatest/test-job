877df53745537140dd42cc9f3b338d9383494bdd
Sevastyan Kalgin <foxhable@gmail.com>
2026-06-10 11:27:52 +0300

    refactor: delete unused js files

--- src/blocks/course-card/course-card.js
+++ src/blocks/course-card/course-card.js
@@ -1,19 +1,16 @@
-import {
-    COURSE_CARD_TEMPLATE_SELECTOR, COURSE_TYPE, PAGE_GET_PARAM, PAGINATION_PER_PAGE,
-} from 'src/constants/courses.js'
+/** @typedef {'marketing' | 'hr' | 'development' | 'design' | 'management'} CourseType */
 
-/** @typedef {{
- * title: HTMLElement,
- * image: HTMLSourceElement,
- * tag: HTMLElement,
- * price: HTMLElement,
- * author: HTMLElement,
- * }} CourseFields */
-
-export const coursesListElement = document.querySelector('.courses-list-section__list')
+/** @type {Record<Uppercase<CourseType>, CourseType>} */
+export const COURSE_TYPE = {
+    MANAGEMENT: 'management',
+    HR: 'hr',
+    DEVELOPMENT: 'development',
+    DESIGN: 'design',
+    MARKETING: 'marketing',
+}
 
 /**
- * @param {import('constants/courses.js').CourseType} courseType
+ * @param {CourseType} courseType
  * @return {string}
  * */
 function getTagClassByCourseType(courseType) {
@@ -32,57 +29,13 @@ function getTagClassByCourseType(courseType) {
     }
 }
 
-/**
- * @param course {Course}
- * @return {HTMLElement | null}
- * */
-export function createCourseCard(course) {
-    const template = document.querySelector(COURSE_CARD_TEMPLATE_SELECTOR)
-    if (!template || !coursesListElement) return
-
-    const card = document.createElement('li')
-    card.className = 'card course-card'
-    card.innerHTML = template.innerHTML
-
-    /** @type {[keyof Exclude<Course, "type">]} */
-    const fields = ['title', 'image', 'price', 'author', 'tag']
-
-    /** @type {CourseFields} */
-    const fieldElements = Object.fromEntries(
-        fields.map((f) => {
-            const el = card.querySelector(`[data-field=${f}]`)
-            return [f, el]
-        }),
-    )
-
-    if (!fields.every((f) => fieldElements[f])) return
-
-    fieldElements.title.innerText = course.title
-    fieldElements.tag.innerText = course.tag
-    fieldElements.image.srcset = course.image
-    fieldElements.tag.classList.add(getTagClassByCourseType(course.type))
-    fieldElements.price.innerText = course.price
-    fieldElements.author.innerText = `by ${course.author}`
-
-    return card
-}
-
-export function clearCoursesList() {
-    coursesListElement.innerHTML = ''
-}
-
-/**
- * @param {Course[]} courses
- * */
-export function mountCoursesListToDOM(courses) {
-    const searchParams = new URLSearchParams(window.location.search)
-    const currentPage = Number(searchParams.get(PAGE_GET_PARAM) || '1')
-
-    courses
-      .slice(0, PAGINATION_PER_PAGE * currentPage)
-      .map(createCourseCard)
-      .forEach((el) => coursesListElement.append(el))
-}
+/** @typedef {{
+ * title: HTMLElement,
+ * image: HTMLSourceElement,
+ * tag: HTMLElement,
+ * price: HTMLElement,
+ * author: HTMLElement,
+ * }} CourseFields */
 
 /** @typedef  {ListSectionCard & {
  * title: string,
